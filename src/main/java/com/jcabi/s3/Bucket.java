ad3a21d658d2958577aa3bd2d07cfb1f968390dd
Alexey Saenko <darkled@users.noreply.github.com>
2015-01-02 14:46:48 +0300

    #5 fixed checkstyle

--- src/main/java/com/jcabi/s3/Bucket.java
+++ src/main/java/com/jcabi/s3/Bucket.java
@@ -84,8 +84,7 @@ public interface Bucket extends Comparable<Bucket> {
 
     /**
      * Checks if the bucket exists.
-     * @return If the bucket exists <code>true</code>, otherwise
-     * <code>false</code>
+     * @return <code>True</code> if the bucket exists
      */
     boolean exists();
 
