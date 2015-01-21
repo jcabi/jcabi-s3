a1c5e509cbba9791b3aaac3c49d191ae8de8696a
Alexander Paderin <apaderin@andiks.com>
2015-01-21 20:32:33 +0300

    #14 keep it simple

--- src/main/java/com/jcabi/s3/Bucket.java
+++ src/main/java/com/jcabi/s3/Bucket.java
@@ -109,28 +109,16 @@ public interface Bucket extends Comparable<Bucket> {
 
     /**
      * Creates bucket with specified origin bucket and prefix.
-     * <p>Explanation of usage:</p>
+     * <p>The goal:</p>
      * <pre>
-     * 1. Create Region
-     * 2. Get bucket for region with name <code>test</code>
-     * 3. Put down ocket keys into bucket
-     * as text "a/first.txt", "a/b/hello.txt" and "a/b/f/2.txt"
-     * 4. Let's say that Prefix is "a/b"
-     * 5. Create new instance of <code>Prefix</code> with Prefix
-     * 6. Get iterator for all keys started with Prefix, and
-     * <code>Prefix.length()</code> leading symbols cut off
-     * Other keys, don't start with Prefix, will be omitted.
-     * 7. Result keys will be: "hello.txt" and "f/2.txt"
-     * cause:
-     * "a/first.txt" - doesn't match conditions
-     * "a/b/hello.txt" - starts with Prefix and result is the same string
-     * but cut off first <code>Prefix.length</code> symbols
-     * "a/b/f/2.txt" - starts with Prefix and result is the same string but
-     * cut off first <code>Prefix.length</code> symbols
+     *  This class is involved to cut off ocket keys of underlying
+     *  bucket by some string known as prefix
      * </pre>
      * <p>Example of usage:</p>
      * <pre>
-     * final Region region = new MkRegion(this.temp.newFolder());
+     * final Region region = new MkRegion(
+     *     new TemporaryFolder().newFolder()
+     * );
      * final Bucket bucket = region.bucket("test");
      * new Ocket.Text(bucket.ocket("a/first.txt")).write("");
      * new Ocket.Text(bucket.ocket("a/b/hello.txt")).write("");
