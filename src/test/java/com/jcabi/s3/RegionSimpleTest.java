75c2748e4658f96c99351ae5bbbda36f435cf4f8
Yegor Bugayenko <yegor256@gmail.com>
2026-02-05 12:06:38 +0300

    #451: Add RegionSimpleTest for Region.Simple class.

---
+++ src/test/java/com/jcabi/s3/RegionSimpleTest.java
@@ -0,0 +1,80 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2012-2026 Yegor Bugayenko
+ * SPDX-License-Identifier: MIT
+ */
+package com.jcabi.s3;
+
+import java.util.UUID;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import software.amazon.awssdk.services.s3.S3Client;
+
+/**
+ * Test case for {@link Region.Simple}.
+ *
+ * @since 0.1
+ */
+final class RegionSimpleTest {
+
+    @Test
+    void returnsBucketByName() {
+        final S3Client aws = Mockito.mock(S3Client.class);
+        final String name = UUID.randomUUID().toString();
+        MatcherAssert.assertThat(
+            "bucket name did not match the requested name",
+            new Region.Simple(aws).bucket(name).name(),
+            Matchers.equalTo(name)
+        );
+    }
+
+    @Test
+    void returnsAwsClient() {
+        final S3Client aws = Mockito.mock(S3Client.class);
+        MatcherAssert.assertThat(
+            "aws client was not the same instance",
+            new Region.Simple(aws).aws(),
+            Matchers.sameInstance(aws)
+        );
+    }
+
+    @Test
+    void createsRegionWithCredentials() {
+        final Region region = new Region.Simple(
+            UUID.randomUUID().toString(),
+            UUID.randomUUID().toString()
+        );
+        MatcherAssert.assertThat(
+            "region was not created from credentials",
+            region.aws(),
+            Matchers.notNullValue()
+        );
+    }
+
+    @Test
+    void createsRegionWithCustomRegion() {
+        final Region region = new Region.Simple(
+            UUID.randomUUID().toString(),
+            UUID.randomUUID().toString(),
+            "eu-west-1"
+        );
+        MatcherAssert.assertThat(
+            "region was not created with custom AWS region",
+            region.aws(),
+            Matchers.notNullValue()
+        );
+    }
+
+    @Test
+    void returnsBucketBoundToItself() {
+        final S3Client aws = Mockito.mock(S3Client.class);
+        final Region region = new Region.Simple(aws);
+        MatcherAssert.assertThat(
+            "bucket region did not match the originating region",
+            region.bucket(UUID.randomUUID().toString()).region(),
+            Matchers.sameInstance(region)
+        );
+    }
+
+}
